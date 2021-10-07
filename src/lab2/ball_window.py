e7bb51bea02d2f3838bde2bc95c22d02327dcc9c
Capu G <capucine.garcon@centraliens-lille.org>
2021-10-07 23:44:29 +0200

    Ajout de méthodes à la classde BallWindow et réalisations de tests associés

--- src/lab2/ball_window.py
+++ src/lab2/ball_window.py
@@ -3,19 +3,54 @@ import numpy as np
 
 # todo implement, document and test the class
 class BallWindow:
+    """Create a ball with a center and a radius
+    """
+
     def __init__(self, center, radius):
-        """
+        """Initialize the center and the radius of the ball
 
         Args:
             center (array): a point with two coordinates.
             radius (int):
         """
+
         self.center = np.array(center)
         self.radius = radius
 
-    def __contains__(self, point):
-        """This function checks if a points is in the ball or not
-        Args:
-            point ([type]): [description]
+    # tested
+    def __str__(self):
+        """Give a description of the ball
+
+        Returns:
+            string: description with radius and center.
         """
-        all((i - j) < self.radius for i, j in point, self.center)
+        return f"A ball with center= {self.center} and radius= {self.radius}"
+
+    # tested
+    def __contains__(self, point):
+        """Check if a point is in the ball or not
+
+        Args:
+            point (list): coordinates of the point
+        """
+        p = np.array(point)
+        assert len(p) == len(self.center)
+        return np.linalg.norm(p - self.center) <= self.radius
+
+    # tested
+    def volume(self):
+        """Calculate the volume of the ball.
+
+        Returns:
+            str: a sentence with the volume of the ball.
+        """
+        return f"The volume is {round((4/3) * self.radius **3 * np.pi, 2)}"
+
+    # tested
+    def area(self):
+        """Calculate the area/ the curved surface of the ball.
+
+        Returns:
+            string :
+        """
+        return f"The area is {round(4 * np.pi * self.radius **2, 2)}"
---
+++ tests/lab2/test_ball_window.py
@@ -0,0 +1,48 @@
+import numpy as np
+import pytest
+
+from lab2.ball_window import BallWindow
+
+
+def test_raise_type_error_when_something_is_called():
+    with pytest.raises(TypeError):
+        # call_something_that_raises_TypeError()
+        raise TypeError()
+
+
+@pytest.mark.parametrize(
+    "center, radius, expected",
+    [
+        ([2.5, 2.5], 1, "A ball with center= [2.5 2.5] and radius= 1"),
+        ([0, 5, 4], 3, "A ball with center= [0 5 4] and radius= 3"),
+    ],
+)
+def test_box_string_representation(center, radius, expected):
+    assert str(BallWindow(center, radius)) == expected
+
+
+@pytest.fixture
+def ball_1():
+    return BallWindow([0, 5], 2)
+
+
+@pytest.mark.parametrize(
+    "point, expected", [([0, 6], True),],
+)
+def test_contains_ball_1(ball_1, point, expected):
+    is_in = ball_1.__contains__(point)
+    assert is_in == expected
+
+
+@pytest.mark.parametrize(
+    "center, radius, expected", [([1, 2], 1, "The volume is 4.19"),],
+)
+def test_ball_volume(center, radius, expected):
+    assert (BallWindow(center, radius)).volume() == expected
+
+
+@pytest.mark.parametrize(
+    "center, radius, expected", [([1, 2], 1, "The area is 12.57"),],
+)
+def test_ball_area(center, radius, expected):
+    assert (BallWindow(center, radius)).area() == expected
